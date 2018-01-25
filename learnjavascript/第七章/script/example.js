280046522910141daafce77a017db8a2e71a794f
wenxuanhu <35138293+wenxuanhu@users.noreply.github.com>
2018-01-25 16:17:29 +0800

    0

--- learnjavascript/第七章/script/example.js
+++ learnjavascript/第七章/script/example.js
@@ -13,10 +13,24 @@
 // alert(info);
 // }
 // 
+// window.onload=function(){
+// var para=document.createElement("p");
+// var testdiv=document.getElementById("testdiv");
+// testdiv.appendChild(para);
+// var txt=document.createTextNode("Hello World");//创建了一个文本节点
+// para.appendChild(txt);
+// }
+
 window.onload=function(){
-var para=document.createElement("p");
-var testdiv=document.getElementById("testdiv");
-testdiv.appendChild(para);
-var txt=document.createTextNode("Hello World");//创建了一个文本节点
-para.appendChild(txt);
+	var para =document.createElement("p");
+	var txt1=document.createTextNode("This is ");
+	para.appendChild(txt1);
+	var emphasis=document.createElement("em");
+	var txt2=document.createTextNode("my");
+	emphasis.appendChild(txt2);
+	para.appendChild(emphasis);
+	var txt3=document.createTextNode("content");
+	para.appendChild(txt3);
+	var testdiv=document.getElementById("testdiv");
+	testdiv.appendChild(para);
 }
