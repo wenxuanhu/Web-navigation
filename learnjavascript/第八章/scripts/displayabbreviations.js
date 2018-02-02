cd791c3de0d7936cee02298922de226358242d5c
wenxuanhu <35138293+wenxuanhu@users.noreply.github.com>
2018-02-02 10:39:43 +0800

    1

--- learnjavascript/第八章/scripts/displayabbreviations.js
+++ learnjavascript/第八章/scripts/displayabbreviations.js
@@ -8,3 +8,20 @@ function displayAbbreviations(){
 		defs[key]=definition;//把title的内容连接到缩略语上。
 	}
 }
+var dlist=document.createElement("dl");
+for(key in defs){
+	var definition=defs[key];
+	var dtitle=document.createElement("dt");
+	var dtitle_text=document.createTextNode(key);
+	dtitle.appendChild(dtitle_text);
+	var ddesc=document.createElement("dd");
+	var ddesc_text=document.createTextNode(definition);
+	ddesc.appendChild(ddesc_text);
+	dlist.appendChild(dtitle);
+	dlist.appendChild(ddesc);
+}
+var header=document.createElement("h2");
+var header_text=document.createTextNode("abbreviations");
+header.appendChild(header_text);
+document.body.appendChild(dlist);
+window.onload.displayAbbreviations;
